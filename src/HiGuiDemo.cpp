30865b9caabff382a530c3c804ce13fb0c92fad5
rbdev7 <roy.burgess7@outlook.com>
2024-02-18 13:18:13 +0000

    Add project files.

---
+++ src/HiGuiDemo.cpp
@@ -0,0 +1,12 @@
+﻿// HiGuiDemo.cpp : Defines the entry point for the application.
+//
+
+#include "HiGuiDemo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
